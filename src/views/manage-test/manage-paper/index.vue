ab97172613e7ca22743ec3c60449e0ee98f64cb7
zhouxingyu88 <33973737+zhouxingyu88@users.noreply.github.com>
2021-04-24 15:45:18 +0800

    update: 调整考试管理结构（调换原来的考试和考卷管理页面index文件）

--- src/views/manage-test/manage-paper/index.vue
+++ src/views/manage-test/manage-paper/index.vue
@@ -11,41 +11,37 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="考试ID" width="95">
+      <el-table-column align="center" label="试卷ID" width="95">
         <template slot-scope="scope">
-          {{ scope.row.testOptionId }}
+          {{ scope.$index }}
         </template>
       </el-table-column>
-      <el-table-column label="考试名称">
+      <el-table-column label="试卷名称">
         <template slot-scope="scope">
-          {{ scope.row.testOptionName }}
+          {{ scope.row.title }}
         </template>
       </el-table-column>
-      <el-table-column label="选择题数目" width="110" align="center">
+      <el-table-column label="开始时间" width="110" align="center">
         <template slot-scope="scope">
-          {{ scope.row.selectNum }}
-        </template>
-      </el-table-column>
-      <el-table-column label="判断题数目" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.judgeNum }}
-        </template>
-      </el-table-column>
-      <el-table-column label="简答题数目" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.qaNum }}
+          <span>{{ scope.row.pageviews }}</span>
         </template>
       </el-table-column>
       <el-table-column label="时长" width="110" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.duration }}</span>
+          {{ scope.row.pageviews }}
         </template>
       </el-table-column>
+      <el-table-column label="总分" width="110" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.pageviews }}
+        </template>
+      </el-table-column>
+
       <el-table-column align="center" prop="created_at" label="操作" width="200">
         <template slot-scope="scope">
           <el-button-group>
-            <el-button type="primary" icon="el-icon-edit" @click="onEditClicked(scope.row.testOptionId, scope.$index)" />
-            <el-button type="danger" icon="el-icon-delete" @click="onDeleteClicked(scope.row.testOptionId, scope.$index)" />
+            <el-button type="primary" icon="el-icon-edit" @click="onEditClicked(scope.$index, scope.$index)" />
+            <el-button type="danger" icon="el-icon-delete" @click="onDeleteClicked(scope.$index, scope.$index)" />
           </el-button-group>
         </template>
       </el-table-column>
@@ -59,20 +55,17 @@
       @close="wordsDialog.visible = false"
     >
       <el-form :model="form">
-        <el-form-item label="考试名称" label-width="120px">
-          <el-input v-model="form.paperName" autocomplete="off" />
+        <el-form-item label="试卷名称" label-width="120px">
+          <el-input v-model="form.examName" autocomplete="off" />
         </el-form-item>
-        <el-form-item label="选择题数目" label-width="120px">
-          <el-input v-model="form.paperSelectNum" autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="判断题数目" label-width="120px">
-          <el-input v-model="form.paperJudgeNum" autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="简答题数目" label-width="120px">
-          <el-input v-model="form.paperQaNum" autocomplete="off" />
+        <el-form-item label="开始时间" label-width="120px">
+          <el-input v-model="form.startTime" autocomplete="off" />
         </el-form-item>
         <el-form-item label="时长" label-width="120px">
           <el-input v-model="form.duration" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="总分" label-width="120px">
+          <el-input v-model="form.totalScore" autocomplete="off" />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -86,7 +79,8 @@
 </template>
 
 <script>
-import { getList, addPaper, changePaperById, deletePaperById, submitWordsDialogResult } from '@/api/test/paper'
+import { getList } from '@/api/table'
+import { deleteExamById, submitWordsDialogResult } from '@/api/test/exam'
 
 export default {
   data() {
@@ -99,13 +93,10 @@ export default {
         changeMode: 'add' // has two value: 'add' and 'update'
       },
       form: {
-        index: -1,
-        paperId: -1,
-        paperName: '',
-        paperSelectNum: 0,
-        paperJudgeNum: 0,
-        paperQaNum: 0,
-        duration: 0
+        examName: '',
+        startTime: null,
+        duration: 0,
+        totalScore: 0
       }
     }
   },
@@ -116,89 +107,47 @@ export default {
     fetchData() {
       this.listLoading = true
       getList().then(response => {
-        this.list = response.data.responseMap.result
+        this.list = response.data.items
         this.listLoading = false
       })
     },
     onCreateNewClicked() {
-      this.wordsDialog.title = '新增考试'
-      this.form.paperName = ''
-      this.form.paperSelectNum = 0
-      this.form.paperJudgeNum = 0
-      this.form.paperQaNum = 0
-      this.form.duration = 0
+      this.wordsDialog.title = '新增试卷'
+      this.form.examName = ''
+      this.form.startTime = ''
+      this.form.duration = ''
+      this.form.totalScore = ''
       this.wordsDialog.visible = true
       this.wordsDialog.changeMode = 'add'
+
+      // var parsedobj = JSON.parse(JSON.stringify(this.form))
     },
-    onDeleteClicked(paper_id, paper_index) {
-      deletePaperById({ testOptionId: paper_id }).then(response => {
-        if (response.data.status === 200) {
-          console.log('delete paper success')
-          this.list.splice(paper_index, 1)
+    onDeleteClicked(exam_id, exam_index) {
+      deleteExamById(exam_id).then(response => {
+        // if (response.data.result === 200) {
+        // eslint-disable-next-line no-constant-condition
+        if (true) {
+          console.log('delete exam success')
+          this.list.splice(exam_index, 1)
         } else {
-          alert('删除失败')
+          // console.log('删除失败')
         }
       })
     },
-    onEditClicked(paper_id, paper_index) {
-      this.wordsDialog.title = '编辑考试'
-      this.form.paperId = paper_id
-      this.form.index = paper_index
-      this.form.paperName = this.list[paper_index].testOptionName
-      this.form.paperSelectNum = this.list[paper_index].selectNum
-      this.form.paperJudgeNum = this.list[paper_index].judgeNum
-      this.form.paperQaNum = this.list[paper_index].qaNum
-      this.form.duration = this.list[paper_index].duration
+    onEditClicked(exam_id, exam_index) {
+      this.wordsDialog.title = '编辑试卷'
+      this.form.examName = this.list[exam_index].title
+      this.form.startTime = this.list[exam_index].pageviews
+      this.form.duration = this.list[exam_index].pageviews
+      this.form.totalScore = this.list[exam_index].pageviews
       this.wordsDialog.visible = true
       this.wordsDialog.changeMode = 'update'
     },
     wordsDialogConfirmOnClicked() {
       const params = {
-        id: this.form.paperId,
-        index: this.form.index,
-        name: this.form.paperName,
-        selectNum: this.form.paperSelectNum,
-        judgeNum: this.form.paperJudgeNum,
-        qaNum: this.form.paperQaNum,
-        duration: this.form.duration,
         changeMode: this.wordsDialog.changeMode
       }
       submitWordsDialogResult(params).then(response => {
-        const index = this.form.index
-        const changeMode = this.wordsDialog.changeMode
-        const temp = {
-          testOptionName: this.form.paperName,
-          selectNum: this.form.paperSelectNum,
-          judgeNum: this.form.paperJudgeNum,
-          qaNum: this.form.paperQaNum,
-          duration: this.form.duration
-        }
-        if (changeMode === 'update') {
-          if (index != null && index >= 0) {
-            const id = this.form.paperId
-            this.list[index].testOptionName = this.form.paperName
-            this.list[index].selectNum = this.form.paperSelectNum
-            this.list[index].judgeNum = this.form.paperJudgeNum
-            this.list[index].qaNum = this.form.paperQaNum
-            this.list[index].duration = this.form.duration
-            changePaperById(id, temp).then(response => {
-              console.log('Updated paper' + temp)
-            })
-          }
-        } else if (changeMode === 'add') {
-          console.log('3')
-          addPaper(temp).then(response => {
-            console.log('Create new paper' + temp)
-            this.list.push({
-              testOptionId: response.data.responseMap.result,
-              testOptionName: this.form.paperName,
-              selectNum: this.form.paperSelectNum,
-              judgeNum: this.form.paperJudgeNum,
-              qaNum: this.form.paperQaNum,
-              duration: this.form.duration
-            })
-          })
-        }
         this.wordsDialog.visible = false
       })
     }
